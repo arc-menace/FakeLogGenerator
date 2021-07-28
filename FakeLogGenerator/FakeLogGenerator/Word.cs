a16a93b7f6c81aacfeb6ec6073c1790c76cf95e0
Joseph Hahn <jmhahn@gmail.com>
2021-07-27 22:01:00 -0500

    Added several ing words and phrases. Setup print code

---
+++ FakeLogGenerator/FakeLogGenerator/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeLogGenerator
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public Word(int id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
